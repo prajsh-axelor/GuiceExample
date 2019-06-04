c127a2fb3136599c6151482150532cdac6e7779d
prajsh <prajsh.axelor@gmail.com>
2019-06-04 19:04:36 +0530

    2nd commit : june 4 || binding in example

--- src/main/java/demo_one/Example.java
+++ src/main/java/demo_one/Example.java
@@ -1,9 +1,29 @@
 package demo_one;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.BindingAnnotation;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
+import com.google.inject.name.Names;
+
+
+
+@BindingAnnotation @Target({FIELD, PARAMETER, METHOD}) @Retention(RUNTIME)
+@interface ApacheRR {}
+@BindingAnnotation @Target({FIELD, PARAMETER, METHOD}) @Retention(RUNTIME)
+@interface ApacheRS {}
+
 
 public class Example {
 
@@ -22,9 +42,23 @@ class ExampleMoudle extends AbstractModule{
 	@Override
 	protected void configure() {
 		// TODO Auto-generated method stub
-		bind(RacingBike.class).to(ApacheRR310.class);
+		bind(RacingBike.class).annotatedWith(ApacheRR.class).to(ApacheRR310.class);
+		bind(RacingBike.class).annotatedWith(ApacheRS.class).to(ApacheRR200.class);
+		bind(RacingBike.class).annotatedWith(Names.named("ApacheRT")).to(ApacheRR160.class);
+		
+		bind(String.class).annotatedWith(Names.named("Prajjwal Singh")).toInstance("This is Prajjwal Singh Welcoming you here");
+		
 //		bind(ApacheRR310.class).to(ApacheRR200.class);
 		
+	}
+	
+	//provides method @Provides
+	@Provides
+	RacingBike provideApache310() {
+		ApacheRR310 apacheRR310 = new ApacheRR310();
+		apacheRR310.setString("This belongs to Prajjwal Singh");
+		System.out.println(apacheRR310.getString());
+		return apacheRR310;
 	}
 	
 }
@@ -32,9 +66,13 @@ class ExampleMoudle extends AbstractModule{
 class ApacheBike {
 	private RacingBike racingBike;
 	
-	@Inject 
-	public ApacheBike(RacingBike racingBike) {
+	@Inject
+	public ApacheBike(RacingBike racingBike) { //calls @Provides method injector  needs its object instance 
+//	public ApacheBike(@Named("ApacheRT") RacingBike racingBike, @Named("Prajjwal Singh") String pj) { //this is instanceBinding
+//	public ApacheBike(@Named("ApacheRT") RacingBike racingBike) { //this is bindingAnnotation using @Named
+//	public ApacheBike(@ApacheRS RacingBike racingBike) { //this is bindingAnnotation using @BindingAnnotation
 		this.racingBike = racingBike;
+//		System.out.println(pj);
 	}
 	
 	public void goRace() {
@@ -42,11 +80,40 @@ class ApacheBike {
 	}
 }
 
+
+//provider class implementation
+class ProviderBikes implements Provider<RacingBike>{
+
+	@Inject
+	public ProviderBikes() {    //has dependencies of its own that's why constructot
+										//with @Inject annotation
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("Inside the constructor of the provide class");
+	}		
+	
+	@Override
+	public RacingBike get() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside get method of provider class!!!");
+		return null;
+	}
+	
+}
+
+
 interface RacingBike{
 	public int begin();
 }
 
 class ApacheRR310 implements RacingBike{
+	private String str ;
+	public void setString(String str) {
+		this.str = str;
+	}
+	public String getString(){
+		return str;
+	}
 	public int begin() {
 		System.out.println("Vrooooommm... and here RR310 goes");
 		return 231;
